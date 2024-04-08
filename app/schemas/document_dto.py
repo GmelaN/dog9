8997d4df464f232ede058eab28b3992133a411c5
gmelan <gmelanals@gmail.com>
2024-04-08 13:45:01 +0900

    refactor: redesigning project structure

--- app/schemas/document_dto.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class DocumentDTO:
-    title: str
-    content: str
-    type: str
-    category: str
-    href: str
--- app/schemas/website_dto.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class WebsiteDTO:
-    name: str
-    url: str
-
-
