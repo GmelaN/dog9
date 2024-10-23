745f21e776944336fd5f9fd488435e27652d87bf
jshyeon <gmelanals@gmail.com>
2024-10-23 15:50:57 +0000

    entity: added SummerizedNews

--- entity/entity.py
+++ entity/entity.py
@@ -2,6 +2,8 @@ from collections import namedtuple
 
 News = namedtuple("News", ["title", "content", "url", "pub_time", "tag", "press", "image"])
 Article = namedtuple("Article", ["title", "content", "url", "pub_time", "tag", "press", "problem", "issue", "keyword"])
+SummerizedNews = namedtuple("SummerizedNews", ["title", "content", "topics", "id"])
+
 
 Journal = namedtuple("Journal", ["journalNm", "journalId"])
 
